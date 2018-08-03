16676a6e65237ac6014b20bae15617a0b7c8aba4
Joleeee <jolejonaseh@gmail.com>
2018-08-03 10:02:54 +0200

    Added global loader class

--- Rammeverk/Loader.cs
+++ Rammeverk/Loader.cs
@@ -14,5 +14,13 @@ namespace Rammeverk
 		{
 			return ContentManager.Load<T>(assetName);
 		}
+
+		public static void LoadContent(ContentManager Content)
+		{
+			ContentManager = Content;
+		}
+
+		static string Particle = "particle";
+		static string Player = "player";
 	}
 }
